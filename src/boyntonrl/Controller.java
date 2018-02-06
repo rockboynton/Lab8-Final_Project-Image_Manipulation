c68c9ab36f5a765f6c2ffe24c9cfb550f29bbeb1
rockboynton <rock.boynton@yahoo.com>
2018-02-05 23:11:26 -0600

    adding .msoe functionality

--- src/boyntonrl/Controller.java
+++ src/boyntonrl/Controller.java
@@ -2,21 +2,22 @@ package boyntonrl;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.logging.Logger;
 
 public class Controller {
 
-//    public static Logger logger = new Logger("bla");
-    private static String imageFilePath;
-    private static String imageFileExtension;
+    private final static Logger LOGGER = Logger.getLogger(Controller.class.getName());
+
+    private static Image image;
+    private static Image tranformedImage = image;
+
 
     @FXML
     private ImageView imageView;
@@ -29,31 +30,98 @@ public class Controller {
                 new FileChooser.ExtensionFilter("Image Files", "*.png",
                         "*.jpg", "*.gif", "*.msoe","*.bmsoe"));
         File file = fileChooser.showOpenDialog(null);
-        ImageIO.read(file);
+        try {
+            image = ImageIO.read(file);
+            imageView.setImage(image);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (IndexOutOfBoundsException e2) {
+            e2.printStackTrace(); // TODO add alert and log
+        }
     }
 
-    public static String getImageFilePath() {
-        return imageFilePath;
+    @FXML
+    private void save(ActionEvent e) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save New Image File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png",
+                        "*.jpg", "*.gif", "*.msoe","*.bmsoe"));
+        File file = fileChooser.showSaveDialog(null);
+        try {
+            ImageIO.write(tranformedImage, file);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (IndexOutOfBoundsException e2) {
+            e2.printStackTrace(); // TODO add alert and log
+        }
     }
 
-    public static void setImageFilePath(String imageFilePath) {
-        Controller.imageFilePath = imageFilePath;
+    @FXML
+    private void reload(ActionEvent e) {
+        imageView.setImage(image);
+        tranformedImage = image;
     }
 
-    public static String getImageFileExtension() {
-        return imageFileExtension;
+    @FXML
+    private void grayscale(ActionEvent e) {
+        WritableImage newImage;
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+         newImage = new WritableImage(width, height);
+        PixelWriter writer = newImage.getPixelWriter();
+        PixelReader reader = image.getPixelReader();
+        Color grayPixel;
+        Color pixel;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                pixel = reader.getColor(x, y);
+                grayPixel = pixel.grayscale();
+                writer.setColor(x,y, grayPixel);
+            }
+        }
+        tranformedImage = newImage;
+        imageView.setImage(tranformedImage);
     }
 
-    public static void setImageFileExtension(String imageFileExtension) {
-        Controller.imageFileExtension = imageFileExtension;
+    @FXML
+    private void red(ActionEvent e) {
+
     }
 
-    public ImageView getImageView() {
-        return imageView;
+    @FXML
+    private void redGray(ActionEvent e) {
+
     }
 
-    public void setImageView(ImageView imageView) {
-        this.imageView = imageView;
+    @FXML
+    private void negative(ActionEvent e) {
+        WritableImage newImage;
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        newImage = new WritableImage(width, height);
+        PixelWriter writer = newImage.getPixelWriter();
+        PixelReader reader = image.getPixelReader();
+        Color negativePixel;
+        Color pixel;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                pixel = reader.getColor(x, y);
+//                double red = pixel.getRed();
+//                double green = pixel.getGreen();
+//                double blue = pixel.getBlue();
+//                double opacity = pixel.getOpacity();
+                negativePixel = pixel.invert();
+                writer.setColor(x,y, negativePixel);
+            }
+        }
+        tranformedImage = newImage;
+        imageView.setImage(tranformedImage);
+    }
+
+    @FXML
+    private void filter(ActionEvent e) {
+
     }
 
     //    @FXML
--- src/boyntonrl/ImageIO.java
+++ src/boyntonrl/ImageIO.java
@@ -1,15 +1,23 @@
 package boyntonrl;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
-import javafx.stage.FileChooser;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
 import java.io.*;
-import java.util.IllegalFormatException;
+import java.util.InputMismatchException;
 
 /**
  * This class is responsible for loading and saving images
  */
 public class ImageIO {
+
+    public static final int HEX = 16;
+
+    private static Image currentImage;
 
     /**
      * Reads in the specified image file and returns a javafx.scene.image.Image object containing
@@ -17,49 +25,89 @@ public class ImageIO {
      * @param file the specified image file
      * @return
      */
-    public static Image read(File file) {
+    public static Image read(File file) throws IndexOutOfBoundsException, IOException {
         Image image;
         String path = file.getPath();
         String extension = path.substring(path.lastIndexOf("."));
-        Controller.setImageFilePath(path);
-        Controller.setImageFileExtension(extension);
         if (extension.equals(".png") || extension.equals(".jpg") || extension.equals(".gif")) { // Java supported image format (.jpg,.png,.gif)
-//            try (FileInputStream stream = new FileInputStream(file)){
-//                image = new Image(stream);
-//            } catch (IOException e1) {
-//                e1.printStackTrace(); // TODO make alert
-//            }
             image = new Image(file.toURI().toString());
         } else if (extension.equals(".msoe")) {
-            readMSOE(file);
+            image = readMSOE(file);
+        } else {
+            throw new IOException();
         }
+        currentImage = image;
         return image;
     }
 
     /**
-     *
-     * @param image
-     * @param file
+     * Writes the specified image to the specified file.
+     * @param image image to be saved
+     * @param file new image file to be created
      * @throws IOException
      */
-    public static void write(Image image, File file) {
+    public static void write(Image image, File file) throws IOException {
         // TODO
+        String path = file.getPath();
+        System.out.println(path);
+        String extension = path.substring(path.lastIndexOf("."));
+        System.out.println(extension);
+        if (extension.equals(".png") || extension.equals(".jpg") || extension.equals(".gif")) { // Java supported image format (.jpg,.png,.gif)
+            javax.imageio.ImageIO.write(SwingFXUtils.fromFXImage(image, null), extension
+                    .substring(1), file);
+            System.out.println("done");
+        } else if (extension.equals(".msoe")) {
+            writeMSOE(image, file);
+        } else {
+            throw new IOException();
+        }
+        currentImage = image;
     }
 
-    private static Image readMSOE(File file) {
+    private static Image readMSOE(File file) throws IndexOutOfBoundsException, IOException {
         // TODO
+        WritableImage image;
+        PixelWriter writer;
+
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            int lineNumber = 0;
-            while ((line = br.readLine()) != null) {
-                lineNumber++;
-                if (lineNumber == 1 && !line.equals("MSOE")) {
-                    throw new IOException();
-                } else if
+            String header = br.readLine();
+            String[] dimensions = br.readLine().split("\\s+");
+            String[] pixels;
+            int width;
+            int height;
+            Color color;
+
+            if (!header.equals("MSOE")) {
+                throw new IOException(); // msoe file not formatted properly
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            width = Integer.parseInt(dimensions[0]);
+            System.out.println(width);
+            height = Integer.parseInt(dimensions[1]);
+            image = new WritableImage(width, height);
+            writer = image.getPixelWriter();
+//            for (int row = 0; row < height; row++) {
+//                pixels = br.readLine().split("\\s+");
+////                System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+//                for (int column = 0; column < width; column++) {
+////                    System.out.println(row + " " + column);
+//                    color = stringToColor(pixels[column]);
+////                    System.out.println(color);
+//                    writer.setColor(column, row, color);
+//                }
+//            }
+            String line = br.readLine();
+            int lineNumber = 0;
+            while (line != null) {
+                pixels = line.split("\\s+");
+                for (int i = 0; i < width; i++) {
+                    color = stringToColor(pixels[i]);
+                    writer.setColor(i, lineNumber, color);
+                }
+                line = br.readLine();
+                lineNumber++;
+            }
         }
+        return image;
     }
 
     private static void writeMSOE(Image image, File file) {
@@ -72,6 +120,21 @@ public class ImageIO {
 
     private static void writeBMSOE(Image image, File file) {
         // TODO
+    }
+
+    private static Color stringToColor(String colorStr) throws InputMismatchException {
+        // checks valid length, # indicating hex, and valid hex color string
+        System.out.println(colorStr);
+        if (colorStr.length() != 9 || colorStr.charAt(0) != '#' || !colorStr.substring(1).matches
+                ("[0-9A-F]+")) {
+            throw new InputMismatchException("invalid hex color");
+        }
+        return new Color(
+                (double) Integer.valueOf(colorStr.substring(1, 3), HEX) / 255,
+                (double) Integer.valueOf(colorStr.substring(3, 5), HEX) / 255,
+                (double) Integer.valueOf(colorStr.substring(5, 7), HEX) / 255,
+                (double) Integer.valueOf(colorStr.substring(7, 9), HEX) / 255) ;
+
     }
 
 }
